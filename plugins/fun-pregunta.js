ec304cdc4a74e85cc7c821abcf200973113afdc2
Adrianmaze <anuardavid2728@gmail.com>
2024-12-04 00:13:13 -0400

    Update fun-pregunta.js

--- plugins/fun-pregunta.js
+++ plugins/fun-pregunta.js
@@ -1,12 +1,24 @@
-let handler = async (m, { command, text }) => m.reply(`
-*⁉️ 𝐏𝐑𝐄𝐆𝐔𝐍𝐓𝐀𝐒 ⁉️*
-  
-*𝙿𝚁𝙴𝙶𝚄𝙽𝚃𝙰:* ${text}
-*𝚁𝙴𝚂𝙿𝚄𝙴𝚂𝚃𝙰:* ${['Si','Tal vez sí','Posiblemente','Probablemente no','No','Imposible'].getRandom()}
-`.trim(), null, m.mentionedJid ? {
-mentions: m.mentionedJid
-} : {})
-handler.help = ['pregunta *<texto>*']
+var handler = async (m, { conn, text, usedPrefix, command }) => {
+
+if (!text) return conn.reply(m.chat, `🚩 *Ingrese un texto a preguntar*\n\n🌱 Ejemplo: ${usedPrefix + command} ¿Hoy estallaremos algo?`, m, rcanal, )
+
+await m.react('❔')
+await delay(1000 * 1)
+await m.react('❓')
+await delay(1000 * 1)
+await m.react('❔')
+await delay(1000 * 1)
+
+await conn.reply(m.chat, + m.dev + `\n\n•*Pregunta:* ` + text + `\n• *Respuesta:* ` + res, m, rcanal)
+
+}
+handler.help = ['pregunta']
 handler.tags = ['fun']
-handler.command = /^pregunta|preguntas|apakah$/i
+handler.command = ['pregunta','preguntas']
+
+
 export default handler
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+let res = ['Si','Tal vez sí','Posiblemente','Probablemente no','No','Imposible','Por que haces estas preguntas','Por eso te dejo','Para que quieres saber','No te dire la respuesta'].getRandom()
