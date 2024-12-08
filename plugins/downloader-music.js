9b76fe9d0e3411b8ec7a5f10ee8594eb6d12cc1c
Adrianmaze <anuardavid2728@gmail.com>
2024-12-08 15:27:13 -0400

    Update downloader-music.js

--- plugins/downloader-music.js
+++ plugins/downloader-music.js
@@ -112,9 +112,9 @@ if (!delius.status) return m.react("❌");
 const downloadUrl = delius.data.download.url;
 const fileSize = await getFileSize(downloadUrl);
 if (fileSize > LimitVid) {
-await conn.sendMessage(m.chat, { document: { url: downloadUrl }, fileName: `${yt_play[0].title}.mp4`, caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃✧ global.packname\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙` }, { quoted: m });
+await conn.sendMessage(m.chat, { document: { url: downloadUrl }, fileName: `${yt_play[0].title}.mp4`, caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃ 𝐃𝐀𝐑𝐋𝐘 𝐁𝐎𝐓 ᡣ𐭩\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙` }, { quoted: m });
 } else {
-await conn.sendMessage(m.chat, { video: { url: downloadUrl }, fileName: `${yt_play[0].title}.mp4`, caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃✧ global.packname\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙`, thumbnail: yt_play[0].thumbnail, mimetype: 'video/mp4' }, { quoted: m });
+await conn.sendMessage(m.chat, { video: { url: downloadUrl }, fileName: `${yt_play[0].title}.mp4`, caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃ 𝐃𝐀𝐑𝐋𝐘 𝐁𝐎𝐓 ᡣ𐭩\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙`, thumbnail: yt_play[0].thumbnail, mimetype: 'video/mp4' }, { quoted: m });
 }} catch (e1) {
 try {    
 let qu = args[1] || '360'
@@ -123,27 +123,27 @@ const yt = await youtubedl(yt_play[0].url).catch(async _ => await youtubedlv2(yt
 const dl_url = await yt.video[q].download()
 const ttl = await yt.title
 const size = await yt.video[q].fileSizeH
-await await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃✧ global.packname\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m })
+await await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃ 𝐃𝐀𝐑𝐋𝐘 𝐁𝐎𝐓 ᡣ𐭩\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m })
 } catch (e2) {
 try {    
 const downloadUrl = await fetch9Convert(yt_play[0].url); 
-await conn.sendMessage(m.chat, { video: { url: downloadUrl }, fileName: `${yt_play[0].title}.mp4`, caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃✧ global.packname\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙`, thumbnail: yt_play[0].thumbnail, mimetype: 'video/mp4' }, { quoted: m });
+await conn.sendMessage(m.chat, { video: { url: downloadUrl }, fileName: `${yt_play[0].title}.mp4`, caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃ 𝐃𝐀𝐑𝐋𝐘 𝐁𝐎𝐓 ᡣ𐭩\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙`, thumbnail: yt_play[0].thumbnail, mimetype: 'video/mp4' }, { quoted: m });
 } catch (e3) {
 try {
 const downloadUrl = await fetchY2mate(yt_play[0].url);
-await conn.sendMessage(m.chat, { video: { url: downloadUrl }, fileName: `${yt_play[0].title}.mp4`, caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃✧ global.packname\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙`, thumbnail: yt_play[0].thumbnail, mimetype: 'video/mp4' }, { quoted: m });
+await conn.sendMessage(m.chat, { video: { url: downloadUrl }, fileName: `${yt_play[0].title}.mp4`, caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃ 𝐃𝐀𝐑𝐋𝐘 𝐁𝐎𝐓 ᡣ𐭩\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙`, thumbnail: yt_play[0].thumbnail, mimetype: 'video/mp4' }, { quoted: m });
 } catch (e4) {
 try {
 const videoInfo = await fetchInvidious(yt_play[0].url)
 const downloadUrl = videoInfo.videoFormats.find(format => format.mimeType === "audio/mp4").url;
-await conn.sendMessage(m.chat, { video: { url: downloadUrl }, fileName: `${yt_play[0].title}.mp4`, caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃✧ global.packname\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙`, thumbnail: yt_play[0].thumbnail, mimetype: 'video/mp4' }, { quoted: m });
+await conn.sendMessage(m.chat, { video: { url: downloadUrl }, fileName: `${yt_play[0].title}.mp4`, caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃ 𝐃𝐀𝐑𝐋𝐘 𝐁𝐎𝐓 ᡣ𐭩\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙`, thumbnail: yt_play[0].thumbnail, mimetype: 'video/mp4' }, { quoted: m });
 } catch (e5) {
 try {
 let searchh = await yts(yt_play[0].url)
 let __res = searchh.all.map(v => v).filter(v => v.type == "video")
 let infoo = await ytdl.getInfo('https://youtu.be/' + __res[0].videoId)
 let ress = await ytdl.chooseFormat(infoo.formats, { filter: 'audioonly' })
-conn.sendMessage(m.chat, { video: { url: downloadUrl }, fileName: `${yt_play[0].title}.mp4`, caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃✧ global.packname\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙`, thumbnail: yt_play[0].thumbnail, mimetype: 'video/mp4' }, { quoted: m });
+conn.sendMessage(m.chat, { video: { url: downloadUrl }, fileName: `${yt_play[0].title}.mp4`, caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃ 𝐃𝐀𝐑𝐋𝐘 𝐁𝐎𝐓 ᡣ𐭩\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙`, thumbnail: yt_play[0].thumbnail, mimetype: 'video/mp4' }, { quoted: m });
 } catch (e6) {
 try {
 let d2 = await fetch(`https://exonity.tech/api/ytdlp2-faster?apikey=adminsepuh&url=${yt_play[0].url}`);
@@ -151,9 +151,9 @@ let dp = await d2.json();
 const audiop = await getBuffer(dp.result.media.mp4);
 const fileSize = await getFileSize(dp.result.media.mp4);
 if (fileSize > LimitVid) {
-await conn.sendMessage(m.chat, { document: { url: audiop }, fileName: `${yt_play[0].title}.mp4`, caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃✧ global.packname\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙` }, { quoted: m });
+await conn.sendMessage(m.chat, { document: { url: audiop }, fileName: `${yt_play[0].title}.mp4`, caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃ 𝐃𝐀𝐑𝐋𝐘 𝐁𝐎𝐓 ᡣ𐭩\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙` }, { quoted: m });
 } else {
-await conn.sendMessage(m.chat, { video: { url: audiop }, fileName: `${yt_play[0].title}.mp4`, caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃✧ global.packname\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙`, thumbnail: yt_play[0].thumbnail, mimetype: 'video/mp4' }, { quoted: m });
+await conn.sendMessage(m.chat, { video: { url: audiop }, fileName: `${yt_play[0].title}.mp4`, caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃ 𝐃𝐀𝐑𝐋𝐘 𝐁𝐎𝐓 ᡣ𐭩\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙`, thumbnail: yt_play[0].thumbnail, mimetype: 'video/mp4' }, { quoted: m });
 }} catch (e) {    
 await m.react('❌');
 console.log(e);
@@ -262,7 +262,7 @@ const delius = await apiResponse.json();
 if (!delius.status) return m.react("❌");
 const downloadUrl = delius.data.download.url;
 //const fileSize = await getFileSize(downloadUrl);
-await conn.sendMessage(m.chat, { document: { url: downloadUrl }, fileName: `${yt_play[0].title}.mp4`, caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃✧ global.packname\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙`, thumbnail: yt_play[0].thumbnail, mimetype: 'video/mp4' }, { quoted: m })     
+await conn.sendMessage(m.chat, { document: { url: downloadUrl }, fileName: `${yt_play[0].title}.mp4`, caption: `┍━━━━━━┙❃┕━━━━━━┑\n┃ 𝐃𝐀𝐑𝐋𝐘 𝐁𝐎𝐓 ᡣ𐭩\n┃✧ ${yt_play[0].title}\n┕━━━━━━┑❃┍━━━━━━┙`, thumbnail: yt_play[0].thumbnail, mimetype: 'video/mp4' }, { quoted: m })     
 } catch (e1) {
 try {
 let d2 = await fetch(`https://exonity.tech/api/ytdlp2-faster?apikey=adminsepuh&url=${yt_play[0].url}`);
